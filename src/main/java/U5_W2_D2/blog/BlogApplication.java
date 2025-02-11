7c9f024ed523000199b52b9ea62705e2a9c6290a
Daniel Indrieri <indrieridaniel@gmail.com>
2025-02-11 14:13:10 +0100

    inizio

---
+++ src/main/java/U5_W2_D2/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package U5_W2_D2.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/U5_W2_D2/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package U5_W2_D2.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
